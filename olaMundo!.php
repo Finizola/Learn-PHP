67d7c685498b836ea1a5076d1c6ce0efcb22f655
finizola <leo.finizola@gmail.com>
2015-08-10 19:10:24 -0300

    ola mundo

---
+++ olaMundo!.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>OLA MUNDO!</title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>ola Mundo</h1>";
+        ?>
+    </body>
+</html>
